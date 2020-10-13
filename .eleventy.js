cb1439cbc176445e49a41f2af86571022b09674a
Eric J. Smith <eric@codesmithtools.com>
2020-10-13 10:50:46 -0500

    More cleaning

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,4 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("cyclone-slider-pro");
-  eleventyConfig.addPassthroughCopy("wp-includes");
 };
