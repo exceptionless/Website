7a81ae8746c77328c0225bf59097df38251e3378
Eric J. Smith <eric@codesmithtools.com>
2020-10-13 11:07:47 -0500

    Still more cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,5 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 };
