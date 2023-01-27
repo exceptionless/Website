969bbf03c5ce916ea51e34373c824b3b69bc0112
Blake Niemyjski <bniemyjski@gmail.com>
2023-01-26 20:52:14 -0600

    Added blog post for .NET Client support for blazor

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
-## Exceptionless Marketing Site  
+# Exceptionless Marketing Site
 
-*Under Construction* 
+This repository contains the Exceptionless marketing website, blog and documentation
 
-## Creating New Posts  
+## Creating New Posts
 
-You can manually create a new markdown file for your posts and ensure the front matter is correct, or you can run the following command from within the project directory: 
+You can manually create a new markdown file for your posts and ensure the front matter is correct, or you can run the following command from within the project directory:
 
-`node new_post.js -t ${YOUR TITLE}`  
+```bash
+node new_post.js -t ${YOUR TITLE}
+```
 
-The title should be wrapped in quotes like this: 
+The title should be wrapped in quotes like this:
 
-`npm run new-post "This is a Test Post"`
-
+```bash
+npm run new-post "This is a Test Post"
+```
--- new_post.js
+++ new_post.js
@@ -3,30 +3,32 @@ const { program } = require('commander');
 program.option('-t, --title <title>', 'Post title');
 
 (() => {
+  console.log("creating a new post...");
+
   try {
-    program.parse(process.argv);
-    console.log("creating a new post...")
-    if(!program.title) {
+    program.parse();
+    const options = program.opts();
+    if (!options.title) {
       console.log("New post title required");
       process.exit(1);
     }
-  
+
     const date = new Date();
     const month = date.getMonth() + 1;
     const twoDigitMonth = month.toString().length < 2 ? `0${month.toString()}` : month;
     const day = date.getDate();
     const twoDigitDay = day.toString().length < 2 ? `0${day.toString()}` : day;
     const year = date.getFullYear();
-  
-    const titleForFile = program.title.toLowerCase();
+
+    const titleForFile = options.title.toLowerCase();
     const titleSplit = titleForFile.split(' ').join('-');
-  
+
     const markdownTitle = `${year}-${twoDigitMonth}-${twoDigitDay}-${titleSplit}.md`
-  
+
     exec(`
 cat <<EOF >./content/news/${year}/${markdownTitle}
 ---
-title: ${program.title}
+title: ${options.title}
 date: ${year}-${twoDigitMonth}-${twoDigitDay}
 draft: true
 ---
@@ -35,12 +37,12 @@ draft: true
         console.log(err);
         process.exit(1);
       }
-  
+
       if(stderr) {
         console.log(stderr);
         process.exit(1);
       }
-  
+
       console.log("New post created");
       process.exit(0);
     })
